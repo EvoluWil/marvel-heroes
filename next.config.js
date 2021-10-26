f1fb201c9d7c067ff822c6617720567ae66eb9c8
Willian Rodrigues <57255222+Willian-Rodrigues@users.noreply.github.com>
2021-10-26 11:34:31 -0300

    add nextconfig

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    PUBLIC_KEY: "your public key",
+    TIMESTAMP: "generate timestamp",
+    HASH: "generate hash md5 (ts+privatekey+publickey)",
+  },
+};
